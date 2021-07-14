de12db9f053c85617c0a3256d447e7f9729f965a
guozhuang <Guo15210875897>
2021-07-14 18:17:46 +0800

    use eureka service

--- src/main/java/com/example/demo/controller/EurekaCliController.java
+++ src/main/java/com/example/demo/controller/EurekaCliController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -33,5 +35,45 @@ public class EurekaCliController {
 
         System.out.println("------------------------------------");
         return list;
+    }
+
+    @GetMapping("/eureka/instance")
+    public List<ServiceInstance> getEurekaInstance(){
+        String serviceId = "micr-service-user";
+        List<ServiceInstance> list = discoveryClient.getInstances(serviceId);
+        System.out.println("------------------------------------");
+        if (list != null && list.size() > 0) {
+            for (ServiceInstance instance : list) {
+                System.out.println("服务Id : " + instance.getServiceId());
+                System.out.println("host : " + instance.getHost());
+                System.out.println("服务port : " + instance.getPort());
+            }
+        } else {
+            System.out.println("注册中心无服务实例");
+        }
+
+        System.out.println("------------------------------------");
+        return list;
+    }
+
+    //通过eureka调用具体逻辑
+    @GetMapping("/eureka/user")
+    public User getUserInfo(){
+        String serviceId = "micr-service-user";
+        List<ServiceInstance> list = discoveryClient.getInstances(serviceId);
+        System.out.println("------------------------------------");
+        if (list != null && list.size() > 0) {
+            int size = list.size() - 1;
+            int index = (int)(0+Math.random()*(size-0+1));
+
+            ServiceInstance instance = list.get(index);
+            String url = "http://" + instance.getHost() + ":" + instance.getPort()
+                    + "/1";
+
+            System.out.println(url);
+            return this.restTemplate.getForObject(url, User.class);
+        }
+
+        return null;
     }
 }
